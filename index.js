1d586f6c13a08ce6bd3694b0c28ee6b1a170104b
MNP-GH-399724 <399724@LT001694-399724>
2023-12-28 09:39:58 +0530

    day2 page arranged and structuring

--- index.js
+++ index.js
@@ -1,59 +1,80 @@
 const express = require('express');
-const cloudinary = require('cloudinary').v2;
+require('dotenv').config()
+const cloudinary = require('./cloudinary/cloudinary.js');
 const bodyParser = require('body-parser')
+
 
 const app = express();
 
 
 //middlewares
-app.use(express.static('public'));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json({limit:'50mb'}))
+//app.use(express.static('public'));
+//app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}))
 
 
-//clodinary configuration
-cloudinary.config({ 
-    cloud_name: 'dn5rxyzjo', 
-    api_key: '686769132462363', 
-    api_secret: 'wZvOez2prXtkgYA9-jKCrkKo-sQ' 
-  });
 
 
 //routes
 app.get('/',(req,res)=> {
-    res.send('welcome home page');
+    res.send('<h2>welcome home page</h2>');
 })
 
-app.post('/image/upload',(req,res)=> {
+app.post('/image/upload',async(req,res)=> {
     // user image
-    const data = {
-        image:req.body.image,
-    }
-    console.log(data);
+        const {image} = req.body;
+    console.log(image);
     console.log('inside upload post');
     
     //upload image 
-    cloudinary.uploader.upload(data.image)
-    .then((result)=> {
-        console.log('success');
-        res.status(200).send({
-            message: "Upload success..!",
-            result,
-        })
-    }).catch((err)=> {
-        console.log('failed');
-        res.status(500).send({
-            message: "Upload failed !!!!!",
-            err,
-        })
-    })
+    //const uploadedImages =image.map(async img=> {
+        const upload = await cloudinary.uploader.upload(image,
+            {
+                upload_preset: 'unsigned_upload',
+                allowed_formats : ['png', 'jpg', 'jpeg', 'svg', 'ico', 'jfif', 'webp'],
+            },
+            function (err,res) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            console.log(upload,'==upload');
+           // return upload
+        //})
+        
+        // try {
+        //     const fullFilled = await Promise.all(uploadedImages).then( vals=> {return vals})
+        //     const publicIDS = fullFilled.map(image=> {
+        //         console.log(image);
+        //         return image.public_id
+        //     })
+        //   console.log(publicIDS);  
+        //   res.status(200).json(publicIDS)
+        // } catch (error) {
+        //     res.status(500).json(error)
+        // }
+
+    // cloudinary.uploader.upload(data.image)
+    // .then((result)=> {
+    //     console.log('success');
+    //     res.status(200).send({
+    //         message: "Upload success..!",
+    //         result,
+    //     })
+    // }).catch((err)=> {
+    //     console.log('failed');
+    //     res.status(500).send({
+    //         message: "Upload failed !!!!!",
+    //         err,
+    //     })
+    // })
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT||4140;
 
 app.listen(PORT,()=> {
     console.log(`server is running on port ${PORT}`);
 })
 
 
-module.exports = app
